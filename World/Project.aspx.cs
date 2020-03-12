f98b6b9b4958e2fbcca8416ea4d05cc5baca42dd
labpatron <labpatron@BUR251-1877-COB>
2020-03-12 19:06:03 -0400

    Project Big

--- World/Project.aspx.cs
+++ World/Project.aspx.cs
@@ -25,11 +25,13 @@ public partial class Project : System.Web.UI.Page
 
     protected void btnViewProject_Click(object sender, EventArgs e)
     {
+        // Button sends to you a different page to view record information
         int index = projectList.SelectedIndex;
         switch (index)
         {
             case 0:
                 Label1.Text = "Project1";
+                Response.Redirect("ProjectPage1.aspx");
                 break;
             case 1:
                 Label1.Text = "Project2";
@@ -48,7 +50,9 @@ public partial class Project : System.Web.UI.Page
 
     protected void btnRemoveTeam_Click(object sender, EventArgs e)
     {
+        // Removes team member from currently selected project
         string username = "";
+        int index = myTeamList.SelectedIndex;
         int userId = Convert.ToInt32(myTeamList.SelectedValue.ToString());
         SqlCommand update = new SqlCommand("update [dbo].[TeamMember] SET ProjectId = NULL where OwnerId = @ownerId and UserId = @userId");
         update.Connection = con;
@@ -56,54 +60,69 @@ public partial class Project : System.Web.UI.Page
         update.Parameters.AddWithValue("@ownerId", (int)Session["userId"]);
         con.Open();
         update.ExecuteNonQuery();
+        /*
+        FROM TeamMember INNER JOIN TeamOwner ON TeamMember.OwnerId = TeamOwner.UserId FULL OUTER JOIN Users ON 
+        TeamMember.UserId = Users.UserId WHERE (TeamOwner.UserId = @UserId) AND (TeamMember.OwnerId = @ownerId)
+        SqlCommand cmd = new SqlCommand("select * FROM TeamMember INNER JOIN TeamOwner ON TeamMember.OwnerId = TeamOwner.UserId FULL OUTER JOIN Users ON 
+        TeamMember.UserId = Users.UserId WHERE (TeamOwner.UserId = @UserId) AND (TeamMember.OwnerId = @ownerId");
+        */
         //SqlCommand cmd = new SqlCommand("select * from [dbo].[TeamMember] where OwnerId = @ownerId and UserId = @userId");
-        //cmd.Connection = con;
-        //cmd.Parameters.AddWithValue("@userId", userId);
-        //cmd.Parameters.AddWithValue("@ownerId", (int)Session["userId"]);
-        //SqlDataReader reader = cmd.ExecuteReader();
-        //if (reader != null && reader.HasRows)
-        //{
-        //    reader.Read();
-        //    username = reader["UserId"].ToString();
-        //    reader.Close();
-        //}
+        SqlCommand cmd = new SqlCommand("SELECT * FROM TeamMember INNER JOIN TeamOwner ON TeamMember.OwnerId = TeamOwner.UserId FULL OUTER JOIN Users ON TeamMember.UserId = Users.UserId WHERE(TeamOwner.UserId = @OwnerId) AND(TeamMember.UserId = @UserId)");
+        cmd.Connection = con;
+        cmd.Parameters.AddWithValue("@UserId", userId);
+        cmd.Parameters.AddWithValue("@OwnerId", (int)Session["userId"]);
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (reader != null && reader.HasRows)
+        {
+            reader.Read();
+            username = reader["Username"].ToString();
+            reader.Close();
+        }
         con.Close();
 
         Label1.Text = "Success";
-        //teamList.AppendDataBoundItems = true;
-        //teamList.Items.Add(new ListItem(username, userId.ToString()));       
+        myTeamList.Items.RemoveAt(index);
+        teamList.AppendDataBoundItems = true;
+        teamList.Items.Add(new ListItem(username, userId.ToString()));
     }
 
     protected void btnRemoveDevice_Click(object sender, EventArgs e)
     {
-
+        // Removes device from currently selected project
         string deviceName = "";
+        int index = myDeviceList.SelectedIndex;
         int deviceId = Convert.ToInt32(myDeviceList.SelectedValue.ToString());
         SqlCommand update = new SqlCommand("update [dbo].[Device] SET ProjectId = NULL where OwnerId = @ownerId and DeviceId = @deviceId");
         update.Connection = con;
         update.Parameters.AddWithValue("@deviceId", deviceId);
         update.Parameters.AddWithValue("@ownerId", (int)Session["userId"]);
-        con.Open();
-        //SqlCommand cmd = new SqlCommand("select * from [dbo].[Device] where OwnerId = @ownerId and DeviceId = @deviceId");
-        //cmd.Connection = con;
-        //cmd.Parameters.AddWithValue("@deviceId", deviceId);
-        //cmd.Parameters.AddWithValue("@ownerId", (int)Session["userId"]);
-        //SqlDataReader reader = cmd.ExecuteReader();
-        //if (reader != null && reader.HasRows)
-        //{
-        //    reader.Read();
-        //    deviceName = reader["Nickname"].ToString();
-        //    reader.Close();
-        //}
+        con.Open();             
+        SqlCommand cmd = new SqlCommand("select * from [dbo].[Device] where OwnerId = @ownerId and DeviceId = @deviceId");
+        cmd.Connection = con;
+        cmd.Parameters.AddWithValue("@deviceId", deviceId);
+        cmd.Parameters.AddWithValue("@ownerId", (int)Session["userId"]);
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (reader != null && reader.HasRows)
+        {
+            reader.Read();
+            deviceName = reader["Nickname"].ToString();
+            reader.Close();
+        }
         update.ExecuteNonQuery();
         con.Close();
         Label1.Text = "Success";
+        
         //deviceList.AppendDataBoundItems = true;
-        //deviceList.Items.Add(new ListItem(deviceName, deviceId.ToString()));       
+        deviceList.Items.Add(new ListItem(deviceName, deviceId.ToString()));
+        myDeviceList.Items.RemoveAt(index);
     }
 
     protected void btnAddTeam_Click(object sender, EventArgs e)
     {
+        // Adds team member to currently selected project
+        string username = "";
+        int index = teamList.SelectedIndex;
+        projectId = Convert.ToInt32(projectList.SelectedValue.ToString());
         int userId = Convert.ToInt32(teamList.SelectedValue.ToString());
         SqlCommand update = new SqlCommand("update [dbo].[TeamMember] SET ProjectId = @projectId where OwnerId = @ownerId and UserId = @userId");
         update.Connection = con;
@@ -111,22 +130,54 @@ public partial class Project : System.Web.UI.Page
         update.Parameters.AddWithValue("@projectId", projectId);
         update.Parameters.AddWithValue("@ownerId", (int)Session["userId"]);
         con.Open();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM TeamMember INNER JOIN TeamOwner ON TeamMember.OwnerId = TeamOwner.UserId FULL OUTER JOIN Users ON TeamMember.UserId = Users.UserId WHERE(TeamOwner.UserId = @OwnerId) AND(TeamMember.UserId = @UserId)");
+        cmd.Connection = con;
+        cmd.Parameters.AddWithValue("@UserId", userId);
+        cmd.Parameters.AddWithValue("@OwnerId", (int)Session["userId"]);
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (reader != null && reader.HasRows)
+        {
+            reader.Read();
+            username = reader["Username"].ToString();
+            reader.Close();
+        }
         update.ExecuteNonQuery();
         con.Close();
+        teamList.Items.RemoveAt(index);
+        myTeamList.AppendDataBoundItems = true;
+        myTeamList.Items.Add(new ListItem(username, userId.ToString()));
         Label1.Text = "Success";
     }
 
     protected void btnAddDevice_Click(object sender, EventArgs e)
     {
+        // Adds device to currently select project
+        int index = deviceList.SelectedIndex;
+        string deviceName = "";
         int deviceId = Convert.ToInt32(deviceList.SelectedValue.ToString());
+        projectId = Convert.ToInt32(projectList.SelectedValue.ToString());
         SqlCommand update = new SqlCommand("update [dbo].[Device] SET ProjectId = @projectId where OwnerID = @userId and DeviceId = @deviceId");
         update.Connection = con;
         update.Parameters.AddWithValue("@deviceId", deviceId);
         update.Parameters.AddWithValue("@projectId", projectId);
         update.Parameters.AddWithValue("@userId", (int)Session["userId"]);
         con.Open();
+        SqlCommand cmd = new SqlCommand("select * from [dbo].[Device] where OwnerId = @ownerId and DeviceId = @deviceId");
+        cmd.Connection = con;
+        cmd.Parameters.AddWithValue("@deviceId", deviceId);
+        cmd.Parameters.AddWithValue("@ownerId", (int)Session["userId"]);
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (reader != null && reader.HasRows)
+        {
+            reader.Read();
+            deviceName = reader["Nickname"].ToString();
+            reader.Close();
+        }
         update.ExecuteNonQuery();
         con.Close();
         Label1.Text = "Success";
+        myDeviceList.AppendDataBoundItems = true;
+        myDeviceList.Items.Add(new ListItem(deviceName, deviceId.ToString()));
+        deviceList.Items.RemoveAt(index);
     }
 }
---
+++ World/ProjectPage1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeFile="ProjectPage1.aspx.cs" Inherits="ProjectPage1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Hello</h1>
+
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="RecordID" DataSourceID="ProjectPage1Grid">
+        <Columns>
+            <asp:BoundField DataField="RecordID" HeaderText="RecordID" InsertVisible="False" ReadOnly="True" SortExpression="RecordID" />
+            <asp:BoundField DataField="TimeStamp" HeaderText="TimeStamp" SortExpression="TimeStamp" />
+            <asp:BoundField DataField="Weight" HeaderText="Weight" SortExpression="Weight" />
+            <asp:BoundField DataField="Temperature" HeaderText="Temperature" SortExpression="Temperature" />
+            <asp:BoundField DataField="Humidity" HeaderText="Humidity" SortExpression="Humidity" />
+            <asp:BoundField DataField="Media_Filepath" HeaderText="Media_Filepath" SortExpression="Media_Filepath" />
+            <asp:BoundField DataField="Length" HeaderText="Length" SortExpression="Length" />
+            <asp:BoundField DataField="Science_Name" HeaderText="Science_Name" SortExpression="Science_Name" />
+            <asp:BoundField DataField="Common_Name" HeaderText="Common_Name" SortExpression="Common_Name" />
+            <asp:BoundField DataField="Validate_Status" HeaderText="Validate_Status" SortExpression="Validate_Status" />
+            <asp:BoundField DataField="DeviceID" HeaderText="DeviceID" SortExpression="DeviceID" />
+        </Columns>
+    </asp:GridView>
+    <asp:SqlDataSource ID="ProjectPage1Grid" runat="server" ConnectionString="<%$ ConnectionStrings:constr %>" SelectCommand="SELECT Record.RecordID, Record.TimeStamp, Record.Weight, Record.Temperature, Record.Humidity, Record.Media_Filepath, Record.Length, Record.Science_Name, Record.Common_Name, Record.Validate_Status, Record.DeviceID FROM Record INNER JOIN Device ON Record.DeviceID = Device.DeviceID WHERE (Device.ProjectId = @projectId)">
+        <SelectParameters>
+            <asp:SessionParameter Name="projectId" SessionField="projectId" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+</asp:Content>
+
